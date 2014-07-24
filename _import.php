3ed0f080c7e748724b3ee7bd01a6b6312ae17969
Juan Jose Fajardo <sientetuinterior@gmail.com>
2014-07-24 20:59:34 +0200

    reordenar funcions per proves de traspas

--- _import.php
+++ _import.php
@@ -3,6 +3,8 @@
 const ARTINPOCKET_CAT    = true;
 const ORIGEN_UPLOADS     = './_obres.cat/'; 
 const DESTINACIO_UPLOADS = './wp-content/uploads/'; 
+const DB_ORIGEN          = 'wpartinpocket.cat';
+
 
 if (ARTINPOCKET_CAT)
 	define('DOMINI' , 'http://www.artinpocket.cat');
@@ -18,159 +20,166 @@ $wpdb_other = new wpdb( DB_USER, DB_PASSWORD, DB_NAME, DB_HOST);
 set_time_limit(0);
 ini_set('memory_limit', '2048M');
 
-// $query = $wpdb_other->prepare('SELECT * FROM wp_postmeta WHERE post_id=%d',2199);
-// $result = $wpdb_other->get_results($query);
-// echo "<ul>";
-// foreach ($result as $obj) :
-//    echo "<li>".$obj->meta_key. "-" . $obj->meta_value."</li>";
-// endforeach;
-// echo "</ul>";
+/////////////////////////////////////////////////////////////////////////
 
-
+importExport($wpdb);
 
 /////////////////////////////////////////////////////////////////////////
 
 
-function importExport()
+function importExport($wpdb)
 {
+$res1 = "";
 
+$dades = getDadesDBOrigen();
 
-$res = "";
-
-// iteracio 1
-
-$dades = getDadesOrigen();
-
-if ($dades->status) != "error")
+if ($dades->status != "error")
 	{
-	foreach ($variable as $key => $value)
+	foreach ($dades->records as $row)
 		{
-		$arrayDades = getDades($dades->records);
-
-		$id_origen = 999;
-		$nomImgOrigen = $id_origen;
-		$nomImgDesti  = "?????????????????";
-		$any = "2015";
-		$mes = "07";
-
+		print_r($row);
+		echo "</br></br>";
+		$postValors = assignaDadesPost($row);
+		print_r($postValors);
+		echo "</br></br>";		
 		$post_ids = addPost( $postValors );
-
 		var_dump($post_ids);
+		addPostMeta( $post_ids, $postValors, assignaDadesPostMeta($row) );
 
-		addPostMeta( $post_ids, $postValors, $postMetaValors );
+		// insertarCategoriaoTag($wpdb, assignaDadesCategoria($post_ids[0], $row) );
+		// insertarCategoriaoTag($wpdb, assignaDadesTag($post_ids[0], $row) );
 
-		insertarCategoriaTag($wpdb, $post_ids[0], 'product_cat', "nom categoria", "descripcio categoria");
-
-		$res = renombraImatge($nomImgOrigen, $nomImgDesti, $any, $mes);
+		$res = renombraImatge( assignaDadesImatges($row, $postValors->attachment[0]->postName) );
 		if ($res != "") $res1 .= $res . ",";
 		}
+	}
 
 
 if ($res1 != "") echo '</br>Error en imatges: ' . $res1;
-
-
-/////////////////////////////////////////////////////////////////////////
-/////////////////////////////////////////////////////////////////////////
-
-function getDades()
-{
-
-$descripcio = "descripcio OBRA";
-$titol      = "titol OBRA";
-$postName[0]   = "Nom del Post-product";
-$postName[1]   = "Nom del Post-attachment-1";
-$postName[2]   = "Nom del Post-attachment-2";
-$postName[3]   = "Nom del Post-attachment-3";
-
-$any[0] = 2014;
-$any[1] = 2014;
-$any[2] = 2014;
-
-$mes[0] = '07';
-$mes[1] = '07';
-$mes[2] = '07';
-
-$filename[0] = $postName[1] . '.jpg';
-$filename[1] = $postName[2] . '_1.jpg';
-$filename[2] = $postName[3] . '_2.jpg';
-
-
-$postValors = (object)array(
-	'postType' 	 => '',
-	'postParent' => 0,
-	'descripcio' => $descripcio,
-	'titol'      => $titol,
-	'excerpt'	 => '',
-	'postName'   => $postName[0],
-	'mimeType'	 => '',
-	'guid'		 => '',
-	'attachment' => array(),
-	);
-
-$postValors->attachment[0] = (object)array(
-	'postName'   => $postName[1],
-	'any'		 => $any[0],
-	'mes'		 => $mes[0],
-	'filename'   => $filename[0]
-	);
-
-$postValors->attachment[1] = (object)array(
-	'postName'   => $postName[2],	
-	'any'		 => $any[1],
-	'mes'		 => $mes[1],
-	'filename'   => $filename[1]
-	);
-
-$postValors->attachment[2] = (object)array(
-	'postName'   => $postName[3],	
-	'any'		 => $any[2],
-	'mes'		 => $mes[2],
-	'filename'   => $filename[2]
-	);
-
-
-$sku       = 'W999';
-$preu      = 999;
-$stock     = 99;
-$voucherId = 1805;
-$pes       = '';
-$llarg     = '';
-$ample     = '';
-$alt       = '';
-
-$postMetaValors = (object)array(
-	'post_id' 		 => 0,
-	'images_gallery' => '',
-	'sku'            => $sku,
-	'preu'           => $preu,
-	'stock'          => $stock,
-	'pes'            => $pes,
-	'llarg'          => $llarg,
-	'ample'          => $ample,
-	'alt'            => $alt	
-	);
-
 }
 
-function getDadesOrigen()
+/////////////////////////////////////////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////
+
+function assignaDadesPost($row)
 {
-	$query = sprintf("",);
-	$response = (object)controlErrorQuery( dbExec($query) );
+
+	$descripcio    = $row->work_description;
+	$titol         = $row->work_name;
+	$postName[0]   = sanitize_title($titol, $row->work_code);
+	$postName[1]   = $postName[0];
+
+	$any[0]        = date("Y", strtotime($row->datetime_upload) );
+	$mes[0]        = date("m", strtotime($row->datetime_upload) );
+	$filename[0]   = $postName[1] . '.jpg';
+
+	$postValors = (object)array(
+		'postType' 	 => '',
+		'postParent' => 0,
+		'descripcio' => $descripcio,
+		'titol'      => $titol,
+		'excerpt'	 => '',
+		'postName'   => $postName[0],
+		'mimeType'	 => '',
+		'guid'		 => '',
+		'attachment' => array(),
+		);
+
+	$postValors->attachment[0] = (object)array(
+		'postName'   => $postName[1],
+		'any'		 => $any[0],
+		'mes'		 => $mes[0],
+		'filename'   => $filename[0]
+		);
+
+	return ($postValors);
+}
+
+function assignaDadesPostMeta($row)
+{
+	$pes       = '';
+	$ample     = '';
+
+	$postMetaValors = (object)array(
+		'post_id' 		 => 0,
+		'images_gallery' => '',
+		'sku'            => ('W' . (($row->work_code) + 100)),
+		'preu'           => $row->work_price,
+		'stock'          => 1,
+		'pes'            => $pes,
+		'llarg'          => $row->work_x_size,
+		'ample'          => $ample,
+		'alt'            => $row->work_y_size	
+		);
+	return ($postMetaValors);
+}
+
+function assignaDadesImatges($row, $postName)
+{
+	$arryDadesImatges = (object)array(
+		'nomImgOrigen' => $row->work_code,
+		'nomImgDesti'  => $postName,
+		'anyImgOrigen' => date("Y", strtotime($row->datetime_upload) ),
+		'mesImgOrigen' => date("m", strtotime($row->datetime_upload) )
+		);
+	return ($arryDadesImatges);
+}
+
+
+function assignaDadesCategoria($post_id, $row)
+{
+	$arryDadesCategoria = (object)array(
+		'id_origen' => $id_origen,
+		'post_id'   => $post_id,		
+		'tipusTerm' => 'product_cat',
+		'nomTerm'   => 'nom_categoria',
+		'descTerm'  => 'descripcio_categoria'
+		);
+	return ($arryDadesCategoria);
+}
+
+function assignaDadesTag($post_id, $row)
+{
+	$arryDadesTag = (object)array(
+		'id_origen' => $id_origen,
+		'post_id'   => $post_id,
+		'tipusTerm' => 'product_tag',
+		'nomTerm'   => 'nom_tag',
+		'descTerm'  => 'descripcio_tag'
+		);
+	return ($arryDadesTag);
+}
+
+
+function getDadesDBOrigen()
+{
+	$query = sprintf("SELECT CONCAT(CONCAT(ap_users.user_name,' '),ap_users.user_second_name) AS nom_artista, 
+							 ap_collections.title AS titol_coleccio,
+							 ap_works.*
+						FROM ap_works
+							INNER JOIN ap_users       ON ap_works.user_code       = ap_users.user_code
+    						LEFT JOIN  ap_collections ON ap_works.collection_code = ap_collections.collection_code
+						WHERE artist_profile = '1' LIMIT 1;");
+	$response = (object)controlErrorQuery( dbExec(DB_ORIGEN, $query) );
 	return ($response);
 }
 
-function renombraImatge($img_id, $nomImg, $any, $mes)
+function renombraImatge( $dadesImatges )
 {
 	$res = "";
-	$imgOrigen = ORIGEN_UPLOADS . $img_id . ".jpg";
-	$imgDesti  = DESTINACIO_UPLOADS . '/' . $any . '/' . $mes . '/' . $nomImg . ".jpg";
+	$imgOrigen = ORIGEN_UPLOADS . $dadesImatges->nomImgOrigen . ".jpg";
+	$imgDesti  = DESTINACIO_UPLOADS . '/' . 
+								$dadesImatges->anyImgOrigen . '/' .
+								$dadesImatges->mesImgOrigen  . '/' .
+								$dadesImatges->nomImgDesti   . ".jpg";
 	if (file_exists($imgOrigen))
 		{
 		$res = rename($imgOrigen, $imgDesti);
-		if ($res==0) $res = $img_id . '.jpg';
+		if (!$res) $res = $dadesImatges->nomImgOrigen . '.jpg';
+		else $res = "";
 		}
 	else
-		$res = $img_id . '.jpg';
+		$res = $dadesImatges->nomImgOrigen . '.jpg';
 	return ($res);
 }
 
@@ -215,7 +224,7 @@ function updateGuid($post_id)
 	$query = sprintf("UPDATE wp_posts SET guid = '%s'
 							WHERE ID='%d';",$attachment,
 											$post_id);
-	$response = controlErrorQuery( dbExec($query,0) );
+	$response = controlErrorQuery( dbExec(DB_NAME, $query,0) );
 	return($response['status']);
 }
 
@@ -252,16 +261,16 @@ function getArrayPost( $postValors )
 
 function addPostMeta($post_ids, $postValors, $postMetaValors)
 {
-	$postMetaValors->post_id = $post_ids[0];
-	$aa = implode ( ',', $post_ids);
-	$postMetaValors->images_gallery = substr($aa, strpos($aa, ',')+1, 99);
+	$postMetaValors->post_id = $post_ids[1];
+	// $aa = implode ( ',', $post_ids);
+	// $postMetaValors->images_gallery = substr($aa, strpos($aa, ',')+1, 99);
 	$postMeta = getArrayPostMeta( $postMetaValors );
 	foreach($postMeta as $key=>$val)
 		{
 		update_post_meta($post_ids[0], $key, $val);
 		echo $key . ' : ' . $val . '<br/>';
 		}
-	$i = 0;
+	$i = 1;
 	foreach ($postValors->attachment as $key => $attData)
 		{
 		$attachment = $attData->any .  '/' . $attData->mes .  '/' . $attData->filename;
@@ -308,32 +317,33 @@ function getArrayPostMeta( $postMetaValors )
 	return ($postMeta);
 }
 
-function insertarCategoriaTag($wpdb, $post_id, $tipusTerm, $term, $descTerm)
+
+function insertarCategoriaTag($wpdb, $dades)
 {
-$tt_array = wp_insert_term(
-  $term, // the term 
-  $tipusTerm, // the taxonomy
-  array(
-    'description'=> $descTerm,
-    'slug' => '',
-    'parent'=> 0
-  )
-);
+	$tt_array = wp_insert_term(
+	  $dades->nomTerm, // the term 
+	  $dades->tipusTerm, // the taxonomy
+	  array(
+	    'description'=> $dades->descTerm,
+	    'slug' => '',
+	    'parent'=> 0
+	  )
+	);
 
-if ($tt_array->error_data)
-	{
-	$tt = $tt_array->error_data;
-	$tt = $tt['term_exists'];
-	}
-else
-	$tt = $tt_array['term_id'];
+	if ($tt_array->error_data)
+		{
+		$tt = $tt_array->error_data;
+		$tt = $tt['term_exists'];
+		}
+	else
+		$tt = $tt_array['term_id'];
 
-$wpdb->insert( $wpdb->term_relationships, array( 'object_id' => $post_id, 'term_taxonomy_id' => $tt ) ); 
-
+	$wpdb->insert( $wpdb->term_relationships, array( 'object_id'        => $dades->post_id, 
+													 'term_taxonomy_id' => $tt )
+												   ); 
 }
 
-
-function dbExec($query, $tipusResultat=1) {
+function dbExec($db, $query, $tipusResultat=1) {
     /* 
      * Executa : string amb consulta SQL
      * Retorna : string JSON
@@ -348,7 +358,7 @@ function dbExec($query, $tipusResultat=1) {
 
     if (!$link) die('Not connected : ' . mysql_error());
 
-    $db_selected = mysql_select_db(DB_NAME, $link);
+    $db_selected = mysql_select_db($db, $link);
 
     if (!$db_selected) {
         die ('No es possible utilitzar la bd: ' . mysql_error());
@@ -420,6 +430,14 @@ function controlErrorQuery($response)
 // }
 // if ($res1 != "") echo '</br>Error en imatges: ' . $res1;
 
+
+// $query = $wpdb_other->prepare('SELECT * FROM wp_postmeta WHERE post_id=%d',2199);
+// $result = $wpdb_other->get_results($query);
+// echo "<ul>";
+// foreach ($result as $obj) :
+//    echo "<li>".$obj->meta_key. "-" . $obj->meta_value."</li>";
+// endforeach;
+// echo "</ul>";
 
 
 ?>
